1fc49ccd2cefac6b00125fbbb44ac00cbe0c3174
Alexander Wang <alexander.yisu.wang@outlook.com>
2024-12-01 20:08:41 -0500

    started server

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
-    println!("Hello, world!");
+use core::panic;
+
+use axum::{routing::get, Router};
+
+async fn hello_world() -> &'static str {
+    println!("[GET] hello_world()");
+    "Hello world!"
+}
+
+#[tokio::main]
+async fn main() {
+    let app: Router<()> = Router::new().route("/", get(hello_world));
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:8080").await.unwrap();
+
+    if let Ok(addr) = listener.local_addr() {
+        println!("Server running on http://{}", addr);
+    } else {
+        panic!("No address binded!");
+    }
+
+    axum::serve(listener, app).await.unwrap();
 }
