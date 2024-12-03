2a8055c93999742e602e2592822101b6f3f4bc25
Logan Bjork <loganbjork@ufl.edu>
2024-12-03 17:54:51 -0500

    meow

--- client/src/routes/+page.svelte
+++ client/src/routes/+page.svelte
@@ -1,3 +1,4 @@
-<h1>Welcome to your library project</h1>
-<p>Create your package using @sveltejs/package and preview/showcase your work with SvelteKit</p>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<div class="w-full h-screen bg-neutral-900">
+  <h1 class="text-6xl text-center text-blue-300">Meow</h1>
+
+</div>
