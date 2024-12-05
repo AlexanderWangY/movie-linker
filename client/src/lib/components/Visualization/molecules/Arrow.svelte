353c57450e08ed5122c5d00645ac98ad303846dd
Logan Bjork <loganbjork@ufl.edu>
2024-12-04 20:02:44 -0500

    visualization complete

---
+++ client/src/lib/components/Visualization/molecules/Arrow.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	interface Props {
+		actors: string[];
+	}
+
+	const { actors }: Props = $props();
+</script>
+
+<div class="flex flex-col items-center justify-center">
+	<div class="h-10 w-1 bg-zinc-500"></div>
+	<div class="text-center my-2">
+		{#each actors as actor}
+			<p class="text-zinc-400">{actor}</p>
+		{/each}
+	</div>
+    <div class="h-10 w-1 bg-zinc-500 relative">
+        <div class="arrow absolute h-3 w-3 rotate-45 bg-zinc-500 top-3/4 left-1/2 -translate-x-1/2"></div>
+    </div>
+</div>
---
+++ client/src/lib/components/Visualization/molecules/MovieCard.svelte
@@ -0,0 +1,11 @@
+<script lang='ts'>
+    interface Props {
+        movie: string
+    }
+
+    const { movie }: Props = $props();
+</script>
+
+<div class="bg-zinc-600 px-5 py-6 min-w-fit max-w-1/2 w-1/2 rounded-lg hover:bg-zinc-800 hover:cursor-pointer">
+    <p class="text-white select-none text-center">{movie}</p>
+</div>
