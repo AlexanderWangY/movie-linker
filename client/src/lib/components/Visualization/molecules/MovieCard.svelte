88e8e6fe5be072eb913e0ae29a202ea5ec6ddc09
Logan Bjork <loganbjork@ufl.edu>
2024-12-04 21:47:11 -0500

    changed card style

--- client/src/lib/components/Visualization/molecules/MovieCard.svelte
+++ client/src/lib/components/Visualization/molecules/MovieCard.svelte
@@ -6,6 +6,6 @@
     const { movie }: Props = $props();
 </script>
 
-<div class="bg-zinc-600 px-5 py-6 min-w-fit max-w-1/2 w-1/2 rounded-lg hover:bg-zinc-700 hover:cursor-pointer">
+<div class="bg-zinc-600 px-5 py-6 min-w-fit max-w-md w-1/2 rounded-lg hover:bg-zinc-700 hover:cursor-pointer">
     <p class="text-white text-xl select-none text-center">{movie}</p>
 </div>
