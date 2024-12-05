62d423adfb1723826750208f5790fb170a31d858
Logan Bjork <loganbjork@ufl.edu>
2024-12-04 23:09:26 -0500

    update

--- client/src/routes/benchmark/+page.svelte
+++ client/src/routes/benchmark/+page.svelte
@@ -1,18 +1,10 @@
-<!-- Imports -->
 <script lang="ts">
-	import SixDegreeScreen from '$lib/components/SixDegrees/SixDegreeScreen.svelte';
-
 	// Supports weights 100-900
 	import '@fontsource-variable/inter';
 	import movies from '$lib/data/output.json';
-	import type { AutocompleteOption } from '@skeletonlabs/skeleton';
 	import { popup } from '@skeletonlabs/skeleton';
 	import type { PopupSettings } from '@skeletonlabs/skeleton';
-	import LinkedList from '../Visualization/LinkedList.svelte';
-	import type { Connection } from '../Visualization/LinkedTypes';
-	import { RadioGroup, RadioItem } from '@skeletonlabs/skeleton';
 	import { ProgressBar } from '@skeletonlabs/skeleton';
-	import { goto } from '$app/navigation';
 
 	const MovieSet = new Set();
 
@@ -22,10 +14,6 @@
 		path: Connection[];
 		time: number;
 		traversal_count: number;
-	}
-
-	interface Path {
-		path: Connection[];
 	}
 
 	let path: Path = {
@@ -41,12 +29,9 @@
 	let movie1_input: string = '';
 	let movie2_input: string = '';
 
-	let bfsObject = {};
-	let dfsObject = {};
-
 	let value: number = 0;
 
-	const fetchBoth = async () => {
+	const fetchConnections = async () => {
 		if (!MovieSet.has(movie1_input) || !MovieSet.has(movie2_input)) {
 			console.log('Error, movies not found');
 			return;
@@ -54,14 +39,12 @@
 
 		path.path = [];
 
-		const data = await fetch(`http://localhost:8080/$bfs?from=${movie1}&to=${movie2}`);
-    const newBFS: Response = await data.json();
+		const res = await fetch(
+			`http://localhost:8080/${value === 1 ? 'dfs_connected' : 'bfs_connected'}?from=${movie1_input}&to=${movie2_input}`,
+			{ mode: 'cors' }
+		);
 
-    const data = await fetch(`http://localhost:8080/dfs?from=${movie1}&to=${movie2}`);
-    const newDFS = await data.json();
-  );
-
-		const data: Response = await data.json();
+		const data: Response = await res.json();
 
 		console.log(data.path);
 
@@ -75,14 +58,6 @@
 		Six Degrees
 		<span class="badge variant-filled text-lg rounded-3xl px-3">Using BFS</span>
 	</div>
-	
-
-	<!-- <div class="flex flex-col items-center justify-center pt-10">
-		<RadioGroup active="bg-neutral-900 text-zinc-300" hover="hover:bg-neutral-400">
-			<RadioItem bind:group={value} name="BFS" value={0}>BFS</RadioItem>
-			<RadioItem bind:group={value} name="DFS" value={1}>DFS</RadioItem>
-		</RadioGroup>
-	</div> -->
 
 	<!-- Autocomplete Search from skeleton lib -->
 	<div class="flex min-h-full max-w-full flex-col items-center p-4 pt-16">
@@ -108,42 +83,48 @@
 
 		<button
 			on:click={fetchConnections}
-			class="mt-10 w-1/4 rounded-lg bg-stone-300 px-5 py-3 hover:cursor-pointer hover:bg-stone-400"
-			>Find!
-		</button>
-
-		<button 
-			on:click={goto('/benchmark')}
-			class="mt-5 w-1/6 rounded-lg bg-green-700 text-stone-200 px-5 py-3 hover:cursor-pointer hover:bg-green-800">
-			Benchmark
+			class="mt-10 w-1/2 rounded-lg bg-stone-300 px-5 py-3 hover:cursor-pointer hover:bg-stone-400"
+			>Calculate benchmark!
 		</button>
 
 	</div>
-
-	<!-- <SixDegreeScreen /> -->
 
 	<div class="flex flex-col items-center gap-y-10 p-24">
 		<h1 class="pb-4 text-3xl font-bold text-zinc-100">Results</h1>
 		<!-- TODO: figure out getting server's response here -->
 		{#if path.path.length !== 0}
-			<LinkedList connections={path} />
+            <div class="mt-auto flex w-full flex-row gap-32 items-center justify-center p-8 pb-32">
+                <div>
+                    <h1 class="text-2xl font-bold text-zinc-100 text-center">BFS RESULTS</h1>
+                      <div class="flex flex-row gap-x-6">
+                        <div class="text-center text-zinc-100">
+                            Time Elapsed (ms):
+                            <div>meow</div>
+                        </div>
+                        <div class="text-center text-zinc-100">
+                            Number of Connections:
+                            <div>meow</div>
+                        </div>
+                      </div>
+                </div>
+                <div>
+                    <h1 class="text-2xl font-bold text-zinc-100 text-center">DFS RESULTS</h1>
+                      <div class="flex flex-row gap-x-6">
+                        <div class="text-center text-zinc-100 font-bold">
+                            Time Elapsed (ms):
+                            <div>meow</div>
+                        </div>
+                        <div class="text-center text-zinc-100 font-bold">
+                            Number of Connections:
+                            <div>meow</div>
+                        </div>
+                      </div>  
+                </div>
+            </div>
 		{:else if ((movie1_input === "") || (movie2_input === ""))}
 			<p class="text-zinc-100 text-lg">Awaiting movies...</p>
 		{:else}
 			<ProgressBar />
 		{/if}
 	</div>
-
-	<!-- <div class="mt-auto flex w-full flex-col items-center justify-center p-8 pb-32">
-		<div class="flex w-full items-center justify-center">
-			<div class="px-20">
-				<div class="text-2xl font-bold text-zinc-100">Previous Guesses<br /></div>
-				<div class="text-center text-zinc-100">placeholder</div>
-			</div>
-			<div class="px-20">
-				<div class="text-2xl font-bold text-zinc-100">Guesses Left<br /></div>
-				<div class="text-center text-zinc-100">placeholder</div>
-			</div>
-		</div>
-	</div> -->
 </div>
